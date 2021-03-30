61c09dc9ca78ec58623323f4100c6feca5706cd1
telinak <78906896+telinak@users.noreply.github.com>
2021-03-31 01:14:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  k = 0;
+  int k = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value)
