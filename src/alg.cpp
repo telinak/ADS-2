74ba3318c9e8011970886340d328aa612b6e259c
telinak <78906896+telinak@users.noreply.github.com>
2021-03-31 01:12:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,7 +10,7 @@ int countPairs1(int *arr, int len, int value) {
   return k;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;    
+  return 0;
 }
 int countPairs3(int *arr, int len, int value) {
   return 0;
