019dd814a41eadd3bc6d5c1edc3979c8beffe2ac
telinak <78906896+telinak@users.noreply.github.com>
2021-03-31 00:58:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,18 @@
-// Copyright 2021 NNTU-CS
-
 int countPairs1(int *arr, int len, int value) {
   k = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value)
-        k = k+1;
+        k = k + 1;
     }
   }
   return k;
 }
-
 int countPairs2(int *arr, int len, int value) {
-  right = len - 1;
-  k = 0;
+  int right = len - 1, k = 0;
   for (int i = len - 1; i > -1; --i) {
     if (arr[i] > value)
-      right -= 1
+      right -= 1;
       else 
         break;
   }
@@ -37,12 +33,11 @@ int countPairs2(int *arr, int len, int value) {
   }
   return k;    
 }
-
 int countPairs3(int *arr, int len, int value) {
   int right = len - 1, k = 0;
   for (int i = len - 1; i > -1; --i) {
     if (arr[i] > value)
-      right -= 1
+      right -= 1;
       else 
         break;
   }
@@ -50,10 +45,10 @@ int countPairs3(int *arr, int len, int value) {
   for (int i = 0; i < right + 1; ++i) {
   while (r > l) {
         int m = (l + r) / 2;
-    if (a[m] < (value - arr[i]) )
+    if (arr[m] < (value - arr[i]) )
       l = m + 1;
-      else 
-        if (a[m] > (value - arr [i]) ) {
+      else {
+        if (arr[m] > (value - arr[i]) ) {
       r = m - 1;
           else 
       k = k + 1;
