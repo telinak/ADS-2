ddf3f0532cee8065a40754fdaaec042ee834fae3
telinak <78906896+telinak@users.noreply.github.com>
2021-03-31 06:29:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
   int right = len - 1, k = 0;
-  for (int i = len - 1; i > -1; —i) {
+  for (int i = len - 1; i > -1; --i) {
     if (arr[i] > value)
       right -= 1;
     else
